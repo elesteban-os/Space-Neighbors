14df8057d241cddbca21fb0bddf0c709104662f6
Esteban <elesteban.os@estudiantec.cr>
2021-06-05 14:09:32 -0600

    Changes by David Leiton

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+#########################################
+
+ Instituto Tecnológico de Costa Rica
+ Área Académica de Ing. en Computadores
+
+ Proyecto de Taller de programación con Python
+
+ Estudiantes:
+ David A. Leitón Flores - 2021103803
+ Kevin E. Chinchilla Rodríguez - 2021101242+
+
+#########################################
+"""
+
+# Librerias a utilizar en este proyecto
+import tkinter as tk
+
+# Creación de la ventana de trabajo.
+window = tk.Tk()
+window.title("Space Neighbors")
+window.minsize(720, 480)
+window.resizable(False, False)
+window.config( bg= "Black")
+window.mainloop()
+
